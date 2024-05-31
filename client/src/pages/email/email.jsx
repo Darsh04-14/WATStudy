2805459620a7d399be0e1bc21cad61a4ae29306a
Sparsh101AI <Sparshpatel6165@gmail.com>
2024-05-31 17:53:27 -0400

    email button fix

--- client/src/pages/email/email.jsx
+++ client/src/pages/email/email.jsx
@@ -6,23 +6,7 @@ const Study = () => {
     const { studySpots, isLoading, error } = useStudySessions({ sessionId: 6 });
 
     const handleSendEmail = () => {
-        if (studySpots && studySpots.length > 0) {
-            const emailAddresses = studySpots.map(spot => spot.email).join(',');
 
-            const sessionDetails = studySpots[0];
-            const emailBody = `
-                Subject: ${sessionDetails.subject}
-                Title: ${sessionDetails.title}
-                Description: ${sessionDetails.description}
-                Date: ${new Date(sessionDetails.session_date).toLocaleString()}
-                Duration: ${sessionDetails.duration} minutes
-                Group Size: ${sessionDetails.group_size}
-                Location: ${sessionDetails.location}
-            `;
-                // for auto adressing
-            const mailtoLink = `mailto:${emailAddresses}?subject=${encodeURIComponent(sessionDetails.title)}&body=${encodeURIComponent(emailBody)}`;
-            window.location.href = mailtoLink;
-        }
     };
 
     if (isLoading) {
