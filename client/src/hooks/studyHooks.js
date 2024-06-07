d9c579f3e618cb9cf12715271ebbe60c15eb6f0e
Darsh04-14 <darshpatelcr7@gmail.com>
2024-06-07 13:10:22 -0400

    Added debounce to search + clean up

--- client/src/hooks/studyHooks.js
+++ client/src/hooks/studyHooks.js
@@ -6,7 +6,11 @@ export const useStudySessions = (filter = {}) => {
     const { data, error, isLoading } = useSWR(
         "/studysession?filter=" + encodeURI(JSON.stringify(filter)),
         async () => {
-            const res = await axios.get(`http://localhost:3800/studysession?filter=${encodeURI(JSON.stringify(filter))}`);
+            const res = await axios.get(
+                `http://localhost:3800/studysession?filter=${encodeURI(
+                    JSON.stringify(filter)
+                )}`
+            );
             return res.data;
         }
     );
@@ -15,13 +19,27 @@ export const useStudySessions = (filter = {}) => {
 };
 
 export const useCreateSession = () => {
-    const { trigger } = useSWRMutation(
-        "/studysession",
+    const { trigger, isMutating } = useSWRMutation(
+        "create/studysession",
         async (url, { arg }) => {
-            axios.post(`http://localhost:3800/studysession`, arg);
-            axios.post(`http://localhost:3800/studysession`, arg);
+            axios.post(`http://localhost:3800/studysession`, {
+                data: arg
+            });
         }
     );
 
-    return { createSession: trigger };
+    return { createSession: trigger, isCreating: isMutating };
+};
+
+export const useDeleteSession = () => {
+    const { trigger, isMutating } = useSWRMutation(
+        "delete/studysession",
+        async (url, { arg }) => {
+            console.log(arg);
+            axios.delete(`http://localhost:3800/studysession`, {
+                data: { id: arg },
+            });
+        }
+    );
+    return { deleteSession: trigger, isDeleting: isMutating };
 };
--- client/src/pages/study/study.jsx
+++ client/src/pages/study/study.jsx
@@ -1,28 +1,23 @@
 import React, { useState } from "react";
-import { useStudySessions } from "../../hooks/studyHooks";
+import { useDeleteSession, useStudySessions } from "../../hooks/studyHooks";
 import { Box, CircularProgress, Button, TextField } from "@mui/material";
+import _ from "lodash";
+import axios from "axios";
 import SessionCard from "../../components/sessionCard/sessionCard";
 import SessionModal from "../../components/sessionModal/sessionModal";
-import axios from "axios";
 
 const Study = () => {
-    const [filter, setFilter] = useState({search: ''});
+    const [filter, setFilter] = useState({ search: "" });
     const { studySpots, isLoading } = useStudySessions(filter);
+    const { deleteSession } = useDeleteSession();
     const [open, setOpen] = useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
 
-    const handleDeleteSession = (sessionId) => { //outside hook
-        axios.delete(`http://localhost:3800/studysession`, {
-            data: { id: sessionId }
-        })
-            .then(response => {
-                console.log(response.data);
-            })
-            .catch(error => {
-                console.error('Error deleting this:', error);
-            });
-    };
+    const handleSearch = (e) =>
+        setFilter({ ...filter, search: e.target.value });
+
+    const debounceSearch = _.debounce(handleSearch, 300);
 
     return (
         <Box>
@@ -30,26 +25,35 @@ const Study = () => {
                 id="outlined-basic"
                 label="Outlined"
                 variant="outlined"
-                onChange={(e) => setFilter({...filter, search: e.target.value})}
+                onChange={debounceSearch}
             />
-            <Box sx={{ display: "flex", width: "100vw", marginBottom: "2vh" }}>
+            <Box
+                sx={{
+                    display: "flex",
+                    width: "100vw",
+                    marginBottom: "2vh",
+                    flexWrap: "row",
+                }}
+            >
                 {isLoading ? (
                     <CircularProgress />
                 ) : (
                     studySpots?.map((session, idx) => (
                         <Box key={idx}>
-                        <SessionCard studySession={session} />
-                            <Button variant="contained" onClick={() => handleDeleteSession(session.id)}>
-                            Delete Session
-                        </Button>
-                    </Box>
-                ))
+                            <SessionCard studySession={session} />
+                            <Button
+                                variant="contained"
+                                onClick={() => deleteSession(session.id)}
+                            >
+                                Delete Session
+                            </Button>
+                        </Box>
+                    ))
                 )}
             </Box>
             <Button variant="contained" onClick={handleOpen}>
                 Make Post
             </Button>
-            <SessionModal open={open} handleClose={handleClose} />
             <SessionModal open={open} handleClose={handleClose} />
         </Box>
     );
