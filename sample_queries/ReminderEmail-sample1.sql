e5b69739093327d1341b04a40d4bc2bd104ac407
Sparsh Patel <48163939+Sparsh101AI@users.noreply.github.com>
2024-07-08 01:13:07 -0400

    Update ReminderEmail-sample1.sql

--- sample_queries/ReminderEmail-sample1.sql
+++ sample_queries/ReminderEmail-sample1.sql
@@ -1,12 +1,11 @@
-SELECT u.email, s.subject, s.title, s.description, s.session_date, s.duration, s.group_size, s.location
-FROM user_table AS u 
-JOIN participants AS p ON u.uid = p.userId 
-JOIN session_table AS s ON p.sessionId = s.id 
-WHERE s.id = ( 
-SELECT p.sessionId 
-FROM participants AS p 
-JOIN session_table AS s ON p.sessionId = s.id 
-WHERE p.userId = 1
-ORDER BY s.session_date
-ASC LIMIT 1 
+SELECT * FROM session_table 
+WHERE subject = 'MATH239'
+AND group_size >= 1
+AND group_size <= 5
+AND duration >= 0
+AND duration <= 90
+AND (
+title LIKE '%%' 
+OR description LIKE '%%' 
+OR location LIKE '%%'
 );
