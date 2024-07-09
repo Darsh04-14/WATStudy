8068bee13cf20511468ec6897de60bcd0e0b46f7
Darsh04-14 <darshpatelcr7@gmail.com>
2024-07-08 20:23:31 -0400

    Added production queries

--- production_queries/TopCourseAndTotalHoursQuery-productoin2.sql
+++ production_queries/TopCourseAndTotalHoursQuery-productoin2.sql
@@ -1,1 +1,12 @@
-z
+SELECT 
+s.subject, 
+SUM(s.duration) AS total_hours, 
+u.name AS creator_name, 
+COUNT(p.userId) AS participant_count
+FROM session_table s
+JOIN user_table u ON s.creator_fk = u.uid
+LEFT JOIN participants p ON s.id = p.sessionId
+WHERE s.creator_fk = 103
+GROUP BY s.subject, u.name
+ORDER BY total_hours DESC
+LIMIT 1;
