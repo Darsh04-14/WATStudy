dbc9533aa71d0d2c42fdc3b1939bf2191bfc8896
Sparsh101AI <Sparshpatel6165@gmail.com>
2024-07-08 20:20:00 -0400

    added

--- production_queries/TopCourseAndTotalHoursQuery-productoin2.sql
+++ production_queries/TopCourseAndTotalHoursQuery-productoin2.sql
@@ -1,12 +1,1 @@
-SELECT u.email, s.subject, s.title, s.description, s.session_date, s.duration, s.group_size, s.location
-FROM user_table AS u 
-JOIN participants AS p ON u.uid = p.userId 
-JOIN session_table AS s ON p.sessionId = s.id 
-WHERE s.id = ( 
-SELECT p.sessionId 
-FROM participants AS p 
-JOIN session_table AS s ON p.sessionId = s.id 
-WHERE p.userId = 3 
-ORDER BY s.session_date
-ASC LIMIT 1 
-);
+z
