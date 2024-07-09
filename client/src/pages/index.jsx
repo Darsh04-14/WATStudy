35d60347ff17033a1ab81eb9b661b11664cddddd
Darsh04-14 <darshpatelcr7@gmail.com>
2024-07-09 10:06:09 -0400

    Fixed export for landing page

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,3 +1,4 @@
 export { default as Study } from './study/study';
 export { default as Email } from './email/email';
 export { default as Datapage } from './datapage/datapage';
+export {default as LandingPage} from './landingPage/LandingPage';
